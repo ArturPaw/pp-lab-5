8e9e3b0cb965c0495ebb7f4453d48f7ba5b8a705
Artur <artur.pawelec@microsoft.wsei.edu.pl>
2024-05-04 11:57:56 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
 
 public class Main {
 
@@ -8,6 +10,12 @@ public class Main {
             Person person = new Person("Jan Kowalski", 30);
             System.out.println("Imię: " + person.getName());
             System.out.println("Wiek: " + person.getAge());
+
+            
+            Messenger messenger = new EmailMessenger();
+
+            
+            messenger.sendMessage("Witaj, " + person.getName() + "!");
         } catch (InvalidAgeException e) {
             System.err.println("Błąd: " + e.getMessage());
         }
